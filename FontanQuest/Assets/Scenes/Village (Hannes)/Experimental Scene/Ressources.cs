12c2d22aeea9f6274f5c8fc4b314899ac2457f56
Salazifel <115412828+Salazifel@users.noreply.github.com>
2022-12-01 22:43:19 +0100

    update RTS
    building houses
    ressource management

--- FontanQuest/Assets/Scenes/Village (Hannes)/Experimental Scene/Ressources.cs
+++ FontanQuest/Assets/Scenes/Village (Hannes)/Experimental Scene/Ressources.cs
@@ -8,16 +8,29 @@ public class Ressources : MonoBehaviour
     public int stone;
     public int food;
     public int gold;
-
     // Start is called before the first frame update
-    void Start()
+    public bool CheckRessources(int woodc, int stonec, int foodc, int goldc)
     {
-        
+        bool affordable = true;
+
+        if (woodc > wood)
+            { affordable = false; }
+        if (stonec > stone)
+            { affordable = false; }
+        if (foodc > food)
+            { affordable = false; }
+        if (goldc > gold)
+            { affordable = false; }
+
+        return affordable;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeRessources(int woodc, int stonec, int foodc, int goldc)
     {
-        
+        wood = wood + woodc;
+        stone = stone + stonec;
+        food = food + foodc;
+        gold = gold + goldc;
     }
 }
+
---
+++ FontanQuest/Assets/Scenes/Village (Hannes)/Experimental Scene/build.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class build : MonoBehaviour
+{
+    public GameObject Button;
+
+    public void show_Button()
+    {
+        Button.SetActive(true);
+    }
+}

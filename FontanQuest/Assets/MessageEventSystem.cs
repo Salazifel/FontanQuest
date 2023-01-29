a901925881875ea32ad140db2ade11941bf1dedc
Salazifel <115412828+Salazifel@users.noreply.github.com>
2023-01-29 11:19:30 +0100

    save system for laura

--- FontanQuest/Assets/MessageEventSystem.cs
+++ FontanQuest/Assets/MessageEventSystem.cs
@@ -6,6 +6,8 @@ public class MessageEventSystem : MonoBehaviour
 {
     private int currentSteps;
 
+    private System.DateTime time_game_started;
+
     private List<string> PlayerData = new List<string>();
 
     private List<bool> SentMessages = new List<bool>();
@@ -33,6 +35,8 @@ public class MessageEventSystem : MonoBehaviour
         {
             currentSteps = 0;
         }
+
+        time_game_started = System.DateTime.Now;
     }
 
     void Update()
@@ -79,7 +83,7 @@ public class MessageEventSystem : MonoBehaviour
     {
         if (pD.Count < 4)
         {
-            Debug.Log("PlayerData.Count < 4");
+            Debug.Log("PlayerData.Count < 5");
             return;
         }
 
@@ -91,11 +95,12 @@ public class MessageEventSystem : MonoBehaviour
         }
         GameFullyLoaded = true;
 
-        Debug.Log("PlayerData loaded: " + this.PlayerData[0] + ", " + this.PlayerData[1] + ", " + this.PlayerData[2] + ", " + this.PlayerData[3]);
+        //Debug.Log("PlayerData loaded: " + this.PlayerData[0] + ", " + this.PlayerData[1] + ", " + this.PlayerData[2] + ", " + this.PlayerData[3]);
     }
 
     public List<string> Send_PlayerData_toSaveGameDataCS()
     {
+        add_time_played();
         return this.PlayerData;
     }
 
@@ -107,6 +112,9 @@ public class MessageEventSystem : MonoBehaviour
         this.PlayerData.Add("false");
         this.PlayerData.Add("0");
         this.PlayerData.Add(System.DateTime.Now.ToString("dd/MM/yyyy"));
+        // create variable System.TimeSpan that is empty
+        System.TimeSpan tmp = new System.TimeSpan(0, 0, 0);
+        this.PlayerData.Add(tmp.ToString());
     }
 
     // structure of PlayerData:
@@ -115,6 +123,7 @@ public class MessageEventSystem : MonoBehaviour
     // 2: EverReachedResourceLimit (true/false)
     // 3: number of steps since last entry to the screen
     // 4: last day played
+    // 5: time played in total
 
     public void set_GameFullyLoaded(bool b)
     {
@@ -128,5 +137,13 @@ public class MessageEventSystem : MonoBehaviour
         {
             this.PlayerData[4] = today;
         }
+    }
+
+    private void add_time_played()
+    {
+        System.TimeSpan elapsedTime = System.DateTime.Now - time_game_started;
+        this.PlayerData[5] = (System.TimeSpan.Parse(this.PlayerData[5]) + elapsedTime).ToString();
+        ResourceContainer.set__time_played(elapsedTime + System.TimeSpan.Parse(this.PlayerData[5]));
+        Debug.Log(ResourceContainer.time_played.ToString());
     }
 }

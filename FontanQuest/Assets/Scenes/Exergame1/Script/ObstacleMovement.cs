9df3b369df3820f64b66350f1d099c559ddc6bd6
shumancheng <Shumancheng0119@gmail.com>
2023-11-11 15:51:08 +0100

    GameSceneUpdate

--- FontanQuest/Assets/Scenes/Exergame1/Script/ObstacleMovement.cs
+++ FontanQuest/Assets/Scenes/Exergame1/Script/ObstacleMovement.cs
@@ -1,13 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour
 {
     public float speed = 5f; // Adjust this to control the speed of the obstacle
 
+    // Update is called once per frame
     void Update()
     {
         Vector3 movement = Vector3.back * speed * Time.deltaTime;
         transform.Translate(movement);
-        Debug.Log(gameObject.name + " transform position: " + transform.position);
+
     }
 }
+
--- FontanQuest/Assets/Scenes/Exergame1/Script/ObstacleSpawner.cs
+++ FontanQuest/Assets/Scenes/Exergame1/Script/ObstacleSpawner.cs
@@ -19,14 +19,12 @@ public class ObstacleSpawner : MonoBehaviour
         }
     }
 
+    // Adjust near and far clipping planes in the camera's settings
+    // Ensure obstacles are spawned at an appropriate Z-position
     void SpawnObstacle()
     {
-        // Calculate the spawn position off-screen
-        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, -10f); // Move along the negative Z-axis (towards the camera)
-
+        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, 0f); // Adjust the Z-position as needed
         Instantiate(obstaclePrefab, spawnPosition, Quaternion.identity);
-        // Debug.Log(obstaclePrefab.name + " spawned at position: " + spawnPosition);
-        Debug.Log(obstaclePrefab.name + " transform position: " + transform.position);
     }
 
 }

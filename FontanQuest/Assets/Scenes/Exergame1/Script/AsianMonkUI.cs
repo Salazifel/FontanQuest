6a6e41b345f590253568795239e5b4dcdf0028fa
shumancheng <Shumancheng0119@gmail.com>
2023-11-13 09:30:04 +0100

    UIManager

---
+++ FontanQuest/Assets/Scenes/Exergame1/Script/AsianMonkUI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsianMonkUI : MonoBehaviour
+{
+    public Canvas StoryCanvas;
+    public Canvas StartMenuCanvas;
+    public Canvas GameCanvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StoryCanvas.gameObject.SetActive(true);
+        StartMenuCanvas.gameObject.SetActive(false);
+        GameCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeToMenu()
+    {
+        StoryCanvas.gameObject.SetActive(false);
+        StartMenuCanvas.gameObject.SetActive(true);
+        GameCanvas.gameObject.SetActive(false);
+    }
+
+    public void ChangeToGame()
+    {
+        StoryCanvas.gameObject.SetActive(false);
+        StartMenuCanvas.gameObject.SetActive(false);
+        GameCanvas.gameObject.SetActive(true);
+    }
+
+
+}

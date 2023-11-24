ff5694b3ea78e86ad776f8072ef32b33b52ed23b
shumancheng <Shumancheng0119@gmail.com>
2023-11-24 14:04:11 +0100

    UpdateTimerController

--- FontanQuest/Assets/Scenes/Exergame1/Script/TimerController.cs
+++ FontanQuest/Assets/Scenes/Exergame1/Script/TimerController.cs
@@ -14,34 +14,34 @@ public class TimerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TimerContainer.SetActive(false);
-        countTimerUI.gameObject.SetActive(false);
-        totalTime = 95f; // Set the total time in seconds (1 minute and 30 seconds)
+        //TimerContainer.SetActive(false);
+        //countTimerUI.gameObject.SetActive(false);
+        totalTime = 90f; // Set the total time in seconds (1 minute and 30 seconds)
         UpdateTimerUI(totalTime);
     }
 
-    // Update is called once per frame
     void Update()
     {
+
         accumTime += Time.deltaTime;
-        int countdownValue = Mathf.CeilToInt(5 - accumTime);
-        countTimerUI.Max = Mathf.Max(0, countdownValue); // Ensure the countdown doesn't go below 0
 
         if (accumTime > 6f)
         {
             TimerContainer.SetActive(true);
-            countTimerUI.gameObject.SetActive(true); // Disable the CountDownUI GameObject
-        }
-        if (totalTime > 0f)
-        {
-            totalTime -= Time.deltaTime;
-            UpdateTimerUI(totalTime);
-        }
-        else if (!TimerContainer.activeSelf)
-        {
-            TimerContainer.SetActive(true);
             countTimerUI.gameObject.SetActive(true);
+
+            if (totalTime > 0f)
+            {
+                totalTime -= Time.deltaTime;
+                UpdateTimerUI(totalTime);
+            }
+            else
+            {
+                TimerContainer.SetActive(false);
+                countTimerUI.gameObject.SetActive(false);
+            }
         }
+
     }
 
     void UpdateTimerUI(float timeInSeconds)

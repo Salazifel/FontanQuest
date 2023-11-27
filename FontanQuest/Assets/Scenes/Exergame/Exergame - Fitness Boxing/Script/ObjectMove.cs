8dd2d1ee03058c27cdbe3022f54cdb6fe6cbf851
shumancheng <Shumancheng0119@gmail.com>
2023-11-27 13:50:07 +0100

    InitialGameScene

---
+++ FontanQuest/Assets/Scenes/Exergame/Exergame - Fitness Boxing/Script/ObjectMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMove : MonoBehaviour
+{
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, speed*Time.deltaTime, 0);
+    }
+}

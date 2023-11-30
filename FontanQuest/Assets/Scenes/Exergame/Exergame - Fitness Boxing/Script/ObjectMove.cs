8102ebcf81ac276a5d56f509574bda4b87b45853
shumancheng <Shumancheng0119@gmail.com>
2023-11-30 19:59:02 +0100

    AddPerfektSign
    and punch png

--- FontanQuest/Assets/Scenes/Exergame/Exergame - Fitness Boxing/Script/ObjectMove.cs
+++ FontanQuest/Assets/Scenes/Exergame/Exergame - Fitness Boxing/Script/ObjectMove.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ObjectMove : MonoBehaviour
 {
     public float speed = 0.5f;
+    public GameObject Perfekt1;
+    public GameObject Perfekt2;
+    [SerializeField] float timeToShowPerfekt = 6f;
+    float accumTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +19,39 @@ public class ObjectMove : MonoBehaviour
     void Update()
     {
         transform.Translate(0, speed*Time.deltaTime, 0);
+    }
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "SmallCube1")
+        {
+            accumTime = 0f;  // Reset accumTime before showing "Perfekt"
+            Perfekt1.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        else if (collision.gameObject.tag == "SmallCube2")
+        {
+            accumTime = 0f;  // Reset accumTime before showing "Perfekt"
+            Perfekt2.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        accumTime += Time.fixedDeltaTime;
+
+        if (Perfekt1.activeSelf && accumTime > timeToShowPerfekt)
+        {
+            Perfekt1.gameObject.SetActive(false);
+        }
+
+        if (Perfekt2.activeSelf && accumTime > timeToShowPerfekt)
+        {
+            Perfekt2.gameObject.SetActive(false);
+        }
     }
 }

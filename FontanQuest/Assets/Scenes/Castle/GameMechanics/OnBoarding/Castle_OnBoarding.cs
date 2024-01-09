e45ec2d217f4c1ad72c2300a0106a8f39fe2762d
Salazifel <hannes@rmgb.de>
2024-01-09 18:10:49 +0100

    onboarding messages added

--- FontanQuest/Assets/Scenes/Castle/GameMechanics/OnBoarding/Castle_OnBoarding.cs
+++ FontanQuest/Assets/Scenes/Castle/GameMechanics/OnBoarding/Castle_OnBoarding.cs
@@ -9,8 +9,14 @@ public class Castle_OnBoarding : MonoBehaviour
     private SaveGameObjects.CastleOnBoardingSystem castleOnBoardingSystem;
     private GameObject RotateButton;
 
+    private MessageWindow messageWindow;
+
+    private int stepCounter = 0;
+
     void Start()
     {
+        messageWindow = GameObject.Find("MessageWindow").GetComponent<MessageWindow>();
+
         onboardingGO = GameObject.Find("On_Boarding_Simulation");
         cameraMovementAnimation = onboardingGO.GetComponent<CameraMovementAnimation>();
 
@@ -30,15 +36,83 @@ public class Castle_OnBoarding : MonoBehaviour
 
     public void StepForwardInAnimation()
     {
+        stepCounter ++;
+
+        if (stepCounter == 1) {
+                messageWindow.SetupMessageWindowByMessageObject(new MessageObjectBlueprint.messageObject(
+                "Willkommen!",
+                "Los lass uns zur Burg gehen. Mache ein paar Schritte",
+                null,
+                null,
+                null,
+                null,
+                "Ok",
+                StartKingOnBoardingSimulation,
+                MessageWindow.Character_options.Character_Male_Rouge_01,
+                AnimationLibrary.Animations.Talk,
+                null
+            ));
+        }
+
+        if (stepCounter == 3) {
+                messageWindow.SetupMessageWindowByMessageObject(new MessageObjectBlueprint.messageObject(
+                "Willkommen!",
+                "Ja, weiter so!",
+                null,
+                null,
+                null,
+                null,
+                "Ok",
+                StartKingOnBoardingSimulation,
+                MessageWindow.Character_options.Character_Male_Rouge_01,
+                AnimationLibrary.Animations.Talk,
+                null
+            ));
+        }
+
+        if (stepCounter == 5) {
+                messageWindow.SetupMessageWindowByMessageObject(new MessageObjectBlueprint.messageObject(
+                "Willkommen!",
+                "Uff, das ist anstrenged. Die Haelfte haben wir",
+                null,
+                null,
+                null,
+                null,
+                "Ok",
+                StartKingOnBoardingSimulation,
+                MessageWindow.Character_options.Character_Male_Rouge_01,
+                AnimationLibrary.Animations.Talk,
+                null
+            ));
+        }
+
+        if (stepCounter == 8) {
+                messageWindow.SetupMessageWindowByMessageObject(new MessageObjectBlueprint.messageObject(
+                "Willkommen!",
+                "Wir haben es fast geschafft!",
+                null,
+                null,
+                null,
+                null,
+                "Ok",
+                StartKingOnBoardingSimulation,
+                MessageWindow.Character_options.Character_Male_Rouge_01,
+                AnimationLibrary.Animations.Talk,
+                null
+            ));
+        }
+
+
+
         if (cameraMovementAnimation.step_to_target(CameraMovementAnimation.StepOptions.step_count, 10) == "Done")
         {
             // end OnBoarding
             GameObject.Find("StepButton").SetActive(false);
             RotateButton.SetActive(true);
 
-            Message_EventSystem.SendMessage(new MessageObjectBlueprint.messageObject(
+            messageWindow.SetupMessageWindowByMessageObject(new MessageObjectBlueprint.messageObject(
                 "Willkommen!",
-                "Du hast es geschafft! Nun bist du bei deiner Burg. Oh, der König scheint nach uns zu rufen.",
+                "Du hast es geschafft! Nun bist du bei deiner Burg. Oh, der Koenig scheint nach uns zu rufen.",
                 null,
                 null,
                 null,

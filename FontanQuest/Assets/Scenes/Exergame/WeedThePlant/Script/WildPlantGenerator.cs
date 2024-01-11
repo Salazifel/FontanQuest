f27393bdc83afba84b87a3fa798aba00fcdd7cbd
shumancheng <Shumancheng0119@gmail.com>
2024-01-11 16:08:22 +0100

    GameSceneHalfCOmpelete

--- FontanQuest/Assets/Scenes/Exergame/WeedThePlant/Script/WildPlantGenerator.cs
+++ FontanQuest/Assets/Scenes/Exergame/WeedThePlant/Script/WildPlantGenerator.cs
@@ -33,7 +33,7 @@ public class WildPlantGenerator : MonoBehaviour
 
     IEnumerator WaitAndStart()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(7f);
         StartCoroutine(GenerateWildPlantCoroutine());
     }
     IEnumerator GenerateWildPlantCoroutine()

55db2540a96b40ddfec3558dab9afc47f4103180
shumancheng <Shumancheng0119@gmail.com>
2024-01-25 18:24:07 +0100

    AddAllHint

--- FontanQuest/Assets/Scenes/Exergame/WeedThePlant/Script/ShowHintRight.cs
+++ FontanQuest/Assets/Scenes/Exergame/WeedThePlant/Script/ShowHintRight.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class ShowHintRight : MonoBehaviour
 {
+    public GameObject Right;
+    [SerializeField] float timeToShowHint = 2f;
+    float accumTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlantA") || collision.gameObject.CompareTag("PlantB"))
+        {
+            accumTime = 0f;
+            Right.gameObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        accumTime += Time.fixedDeltaTime;
+
+        if (Right.activeSelf && accumTime > timeToShowHint)
+        {
+            Right.gameObject.SetActive(false);
+            accumTime = 0f;  
+        }
     }
 }

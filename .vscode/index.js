c66d9024871d18a6a841413e170b4f24af3594ae
shumancheng <Shumancheng0119@gmail.com>
2024-02-03 17:36:14 +0100

    Data Receive On Server

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,20 +1,28 @@
 // require ws module
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({port: 8080}, ()=>{
+const wss = new WebSocket.Server({port: 3000}, ()=>{
     console.log('server started');
 })
 
 //this server we can listen to five event: close, connection, error, headers, listening
 //client has 8 events we can listen to: close, error, message, open, ping, pong, unexpected-response, upgrade
 
-wss.on('connection', (ws)=>{
-    ws.on('message', (data)=>{
-        console.log('data received: ' + data);
+wss.on('connection', (ws) => {
+    ws.on('message', (data) => {
+        /*
+        console.log('data received \n %o', data);
         ws.send(data);
-    })
-})
+        */
+        // Convert Buffer to string
+        const message = data.toString('utf8');
+        console.log('data received \n', message);
+        ws.send(message);
+    });
+});
+
+
 
 // print out msg once we're listening
 wss.on('listening', ()=>{
-    console.log('server is listening to port: 8080');
+    console.log('server is listening to port: 3000');
 })
